3ccb04a4ad4468e40d173301a5927eb7c784737f
Ming Liu <nongnongd@yahoo.com>
2014-08-20 20:42:28 -0400

    mc initial checkin

---
+++ src/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  mc
+//
+//  Created by Lili Lin on 8/20/14.
+//  Copyright (c) 2014 mcode. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("Hello, World!\n");
+    printf("Where is my code?");
+    return 0;
+}
+
